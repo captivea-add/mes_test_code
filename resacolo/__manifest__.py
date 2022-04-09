89d15a0a80f5d66f68ab577b0525b2bc89f92fc5
toutchykipik <90046267+captivea-add@users.noreply.github.com>
2022-04-10 00:50:44 +0200

    essai

--- resacolo/__manifest__.py
+++ resacolo/__manifest__.py
@@ -9,7 +9,7 @@ Description relative à la gestion d’une agence de réservation de séjours .
 """,
   'author': "Toutchy kipik",
   'website': "",
-  'category': 'Event',
+  'category': 'event',
   'version': '0.1',
    'depends': ['base'
                ],
@@ -19,7 +19,7 @@ Description relative à la gestion d’une agence de réservation de séjours .
         'views/participants_views.xml',
         'views/creation_sejour_views.xml',
         'views/tuteurs_views.xml',
-        #'views/student_views.xml',
+        'views/templates.xml',
         #'views/subject_views.xml',
     ],
    'demo': ['demo.xml'],
