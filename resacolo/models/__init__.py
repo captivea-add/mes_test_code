a89e5facd641133b8e36d4a0fc8119c93c3e250e
toutchykipik <90046267+captivea-add@users.noreply.github.com>
2022-04-09 23:23:14 +0200

    del

--- resacolo/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import participants
-from . import tuteurs
-from . import sejours
-# from . import adresse
-
-# TODO Creer les classes et vues ci dessous
-#from . import creation_sejours
-#from . import inscription_sejours
